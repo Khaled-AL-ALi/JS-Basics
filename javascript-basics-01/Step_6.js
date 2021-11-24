22c494a622c8953a69335d53e41cd8d87e15fd4b
khaled Al_ali <khaled2020pro@gmail.com>
2021-11-24 11:08:06 +0200

    finish step6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,4 @@
+let firstNumber = prompt("Enter your first number :");
+let SecondNumber = prompt("Enter your second number :");
+let result=Number(firstNumber)/Number(SecondNumber);
+window.alert("the devision of "+firstNumber+" and " + SecondNumber+" is "+ result);
