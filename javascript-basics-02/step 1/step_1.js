14347905e8287ace72e755b3ecb484306f52cc8c
khaled Al_ali <khaled2020pro@gmail.com>
2021-11-25 15:52:32 +0200

    finish basic02 step 1

---
+++ javascript-basics-02/step 1/step_1.js
@@ -0,0 +1,7 @@
+function changeImage() {
+
+    if ( document.getElementById("image1").src="images/image1.jpg"){document.getElementById("image1").src="images/image1_2.jpg"}
+
+    else  {document.getElementById("image1").src = "images/image1.jpg";}
+    
+}
