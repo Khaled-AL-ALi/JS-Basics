b4d5c275bc4b09d086d6fc8c54e5b49c5dca51a6
khaled Al_ali <khaled2020pro@gmail.com>
2021-11-27 21:51:28 +0200

    finish basic03 step 2

---
+++ javascript-basics-03/step 2/step_2.js
@@ -0,0 +1,5 @@
+function toggle_visibility(id) {
+          if(id== "hide"){document.getElementById("texte").style.display = 'none';}
+    else  if(id== "show"){document.getElementById("texte").style.display = 'block';}
+    else{document.getElementById("texte").style.display = 'block';}
+}
